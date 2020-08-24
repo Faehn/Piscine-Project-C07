7b722749901c1aadbe6c5454c84d0a8d2975361b
Barthelemy SAMZUN <bsamzun@e3r9p2.42.fr>
2020-08-24 20:59:50 +0200

    added main

--- ex04/main.c
+++ ex04/main.c
@@ -1,13 +1,17 @@
-int	ft_convert_base(char *str, char *base_from, char *base_to);
+char	*ft_convert_base(char *str, char *base_from, char *base_to);
 #include <stdio.h>
+#include <stdlib.h>
 
 int main(void)
 {
-	char str[] = "  \t\v\r \t\n \f\t \f\n \f+----+-+--+542a45b2jk5jy15j1t212g";
-	char base_from[] = "0123456789abc";
-	char base_to[] = "0123"
+	char str[] = "  \t\v\r \t\n \f\t \f\n \f+----+-+--+-a648y15j1t212g";
+	char base_from[] = "abcdefghijklmnopqrstuvwxyz";
+	char base_to[] = "0123456789";
+	char *dest;
 
-	printf("%s\n%s\::", str);
-	printf("%s", ft_convert_base(str, base_from, base_to));
+	dest = ft_convert_base(str, base_from, base_to);
+	printf("Nombre original : %s\nDonne en base : %s\nDemande en base : %s\n", str, base_from, base_to);
+	printf("Nombre sorti : %s\n", dest);
+	free(dest);
 	return (0);
 }
